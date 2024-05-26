39be812dd1c5210f089f123eed96c2fdbff8562b
KSkoropad <166766572+KSkoropad@users.noreply.github.com>
2024-05-26 11:35:33 -0400

    Update homework_5.sql

--- 03_homework/homework_5.sql
+++ 03_homework/homework_5.sql
@@ -10,11 +10,25 @@ Remove any trailing or leading whitespaces. Don't just use a case statement for 
 
 Hint: you might need to use INSTR(product_name,'-') to find the hyphens. INSTR will help split the column. */
 
-
+SELECT 
+    product_name,
+    CASE 
+        WHEN INSTR(product_name, '-') = 0 THEN NULL 
+        ELSE TRIM(SUBSTRING(product_name, INSTR(product_name, '-') + 1))
+    END AS description
+FROM product
 
 /* 2. Filter the query to show any product_size value that contain a number with REGEXP. */
 
-
+SELECT 
+    product_name,
+    CASE 
+        WHEN INSTR(product_name, '-') = 0 THEN NULL 
+        ELSE TRIM(SUBSTRING(product_name, INSTR(product_name, '-') + 1))
+    END AS description,
+    product_size
+FROM product
+WHERE product_size REGEXP '[0-9]'
 
 -- UNION
 /* 1. Using a UNION, write a query that displays the market dates with the highest and lowest total sales.
@@ -26,7 +40,27 @@ HINT: There are a possibly a few ways to do this query, but if you're struggling
 3) Query the second temp table twice, once for the best day, once for the worst day, 
 with a UNION binding them. */
 
-
+SELECT *
+FROM (
+    SELECT 
+        DISTINCT market_date,
+        SUM(original_price * quantity) AS total_sales,
+        ROW_NUMBER() OVER (ORDER BY SUM(original_price * quantity) DESC) AS row_max
+    FROM vendor_inventory
+    GROUP BY market_date
+) x
+WHERE row_max = 1
+UNION
+SELECT *
+FROM (
+    SELECT 
+        DISTINCT market_date,
+        SUM(original_price * quantity) AS total_sales,
+        ROW_NUMBER() OVER (ORDER BY SUM(original_price * quantity) ASC) AS row_min
+    FROM vendor_inventory
+    GROUP BY market_date
+)y
+WHERE row_min = 1
 
 -- Cross Join
 /*1. Suppose every vendor in the `vendor_inventory` table had 5 of each of their products to sell to **every** 
@@ -39,7 +73,16 @@ Think a bit about the row counts: how many distinct vendors, product names are t
 How many customers are there (y). 
 Before your final group by you should have the product of those two queries (x*y).  */
 
-
+SELECT
+    v.vendor_name,
+    p.product_name,
+    (vi.original_price*c.customer_count*5) AS total_revenue
+FROM vendor v
+JOIN vendor_inventory vi ON v.vendor_id = vi.vendor_id
+JOIN product p ON vi.product_id = p.product_id
+CROSS JOIN
+    (SELECT COUNT(DISTINCT customer_id) AS customer_count FROM customer) c
+GROUP BY v.vendor_name, p.product_name	
 
 -- INSERT
 /*1.  Create a new table "product_units". 
@@ -47,23 +90,38 @@ This table will contain only products where the `product_qty_type = 'unit'`.
 It should use all of the columns from the product table, as well as a new column for the `CURRENT_TIMESTAMP`.  
 Name the timestamp column `snapshot_timestamp`. */
 
-
+DROP TABLE IF EXISTS product_units;
+CREATE TEMP TABLE product_units AS
+SELECT *
+,CURRENT_TIMESTAMP AS snapshot_timestamp
+FROM product
+WHERE product_qty_type = 'unit'
 
 /*2. Using `INSERT`, add a new row to the product_units table (with an updated timestamp). 
 This can be any product you desire (e.g. add another record for Apple Pie). */
 
-
+DROP TABLE IF EXISTS product_units;
+CREATE TEMP TABLE product_units AS -- wasn't sure if it should be TEMP table, if not it should look like 'CREATE TEMP TABLE product_units AS'
+SELECT *
+,CURRENT_TIMESTAMP AS snapshot_timestamp
+FROM product
+WHERE product_qty_type = 'unit';
+INSERT INTO product_units
+VALUES(24,'Pumpkin Pie','10"',3,'unit',CURRENT_TIMESTAMP)
 
 -- DELETE
 /* 1. Delete the older record for the whatever product you added. 
 
 HINT: If you don't specify a WHERE clause, you are going to have a bad time.*/
 
-
+DELETE FROM product_units
+WHERE product_id = 24
 
 -- UPDATE
 /* 1.We want to add the current_quantity to the product_units table. 
 First, add a new column, current_quantity to the table using the following syntax.
+
+SELECT * 
 
 ALTER TABLE product_units
 ADD current_quantity INT;
@@ -78,4 +136,4 @@ Finally, make sure you have a WHERE statement to update the right row,
 	you'll need to use product_units.product_id to refer to the correct row within the product_units table. 
 When you have all of these components, you can run the update statement. */
 
-
+Still working on it
